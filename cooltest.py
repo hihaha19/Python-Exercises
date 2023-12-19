96172b45d122bba88e05c51ce38451e9c581964f
Michaela Hanková <49369316+hihaha19@users.noreply.github.com>
2023-12-19 13:20:12 +0100

    Update of print statements

--- cooltest.py
+++ cooltest.py
@@ -34,8 +34,6 @@ def parse_cmdline_args(args):
     # Spracujeme argumenty vo dvojiciach
     it = iter(args)
     for arg in it:
-        print("vypisem arg")
-        print(arg)
         if arg.startswith('--'):
             key = arg[2:]  # Odstránenie '--'
             try:
@@ -43,47 +41,70 @@ def parse_cmdline_args(args):
                 value = next(it)
                 cmdline_args[key] = value
             except StopIteration:
-                print(f"Varovanie: Argument {key} je ignorovaný, pretože neobsahuje hodnotu.")
+                print(f"Warning: Argument {key} is ignored because doesnt contain value")
         else:
-            print(f"Varovanie: Argument {arg} nie je platný.")
+            print(f"Warning: Argument {arg} is not valid")
 
     return cmdline_args
 
 
-
-def run_tests():
+def run_tests(cmdline_args):
+    passed_count = 0
+    failed_count = 0
+    skipped_count = 0
     test_files = glob.glob('test_*.py')
     test_modules = [os.path.splitext(os.path.basename(f))[0] for f in test_files]
-    parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument('--dut_ip', help='IP adress of DUT')
-    parser.add_argument('--sky', help='Colour of sky')
-    parser.add_argument('--rain', help='Yes or no')
-    # Vytvorte parser a povedzte mu, aby ignoroval neznáme argumenty
-    # Tu môžete pridať známe argumenty, ak sú nejaké
-    known_args, unknown_args = parser.parse_known_args()
-
-    # Použitie funkcie na spracovanie argumentov príkazového riadku
-    cmdline_args = parse_cmdline_args(sys.argv[1:])
-    print(cmdline_args)
+    print("Collected tests from: " + ', '.join(test_modules))
 
     for module_name in test_modules:
+        header = f"=== {module_name} " + "=" * (50 - len(module_name))
+        print(header)
+
         module = importlib.import_module(module_name)
         functions_in_test_example = getmembers(module, isfunction)
-        print(functions_in_test_example)
 
-        # Now, iterate over the list of function tuples
         for func_name, _ in functions_in_test_example:
-            # Check if the function name starts with 'test_'
-            args_to_send = {}
             if func_name.startswith('test_'):
-                num_of_parameters = 0
+                print(f"{module_name}.{func_name}():")
                 test_func = getattr(module, func_name)
                 sig = signature(test_func)
+                args_to_send = {}
                 for param_name, param in sig.parameters.items():
                     if param_name in cmdline_args.keys():
                         args_to_send[param_name] = cmdline_args[param_name]
+                try:
+                    if args_to_send:
+                        test_func(**args_to_send)
+                    else:
+                        test_func()
+                    print("[PASS]")
+                    passed_count += 1
+                except TestFailure as tf:
+                    print("    [FAIL]")
+                    print(f"    Reason: {tf}")
+                    failed_count += 1
+                except TestSkip as ts:
+                    # Handle test skip exception
+                    print("    [SKIP]")
+                    print(f"    Reason: {ts}")
+                    skipped_count += 1
 
-            # param name su argumenty, ktore potrebujem, prechadzam ich a ked taky najdem v slovniku, tak ho poslem
+        total_count = passed_count + failed_count + skipped_count
+        print(f"Finished. {total_count} total; {passed_count} passed, {failed_count} failed, {skipped_count} skipped.")
 
 
-run_tests()
+def main():
+    print("Starting a new test session with arguments:")
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('--dut_ip', help='IP address of DUT')
+    parser.add_argument('--sky', help='Colour of sky')
+    parser.add_argument('--rain', help='Yes or no')
+    # Parse known arguments
+    # Process command-line arguments
+    cmdline_args = parse_cmdline_args(sys.argv[1:])
+    print(cmdline_args)
+    # Run the tests with the processed command-line arguments
+    run_tests(cmdline_args)
+
+if __name__ == '__main__':
+    main()
